2768584b47583ae1f56e032ec594d061aef6387e
wsh <376220459@qq.com>
2019-05-16 01:27:20 +0800

    修正了登录页面，添加了注册页面，添加了课程页面

--- pages/classes/classes.js
+++ pages/classes/classes.js
@@ -4,12 +4,30 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
-    
-  },
-
-  goBack(){
-    wx.navigateBack();
+    // height: '1045rpx',
+    classes: [
+      {
+        img: '',
+        subject: '大学有机化学实验',
+        title: '--磺基水杨酸合铁组成',
+        time: '8:00-10:15',
+        place: '东区 ff106'
+      },
+      {
+        img: '',
+        subject: '数字电路与逻辑设计',
+        title: '--数字系统设计',
+        time: '12:10-14:15',
+        place: '东区 ff106'
+      },
+      {
+        img: '',
+        subject: '数字电路与逻辑设计',
+        title: '--示波器使用简介',
+        time: '18:10-19:50',
+        place: '西区 b122'
+      }
+    ]
   },
 
   /**
@@ -23,7 +41,10 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+    // let clientHeight = wx.getSystemInfoSync().windowHeight > wx.getSystemInfoSync().windowWidth ? wx.getSystemInfoSync().windowHeight * 2 : wx.getSystemInfoSync().windowWidth*2;
+    // this.setData({
+    //   height: (clientHeight - 111 - 178) + 'rpx'
+    // })
   },
 
   /**
